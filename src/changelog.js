b9560c8cb9a77bdfb1ce660864a4748133b40642
Dan Reynolds <me@danreynolds.ca>
2018-07-04 14:45:33 -0700

    update changelog

--- src/changelog.js
+++ src/changelog.js
@@ -2,4 +2,5 @@ export default [
   'Support for toasts on updates added.',
   'Added support for scrolling through your previous messages by hitting up/down in the chat box.',
   'Added support for role questions like "What roles does Irelia play"',
+  'Added support for recommending champions based on each summoner. Try asking "Who should I play?" and have a good game!'
 ]
